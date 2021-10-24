991f091a5cd8bfb9df0b11451ca0ea8c852fec4b
CsJoy0363 <latestworld9042@gmail.com>
2021-10-24 08:07:43 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more
+          Learn about us
         </a>
       </header>
     </div>
