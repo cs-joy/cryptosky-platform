71038450231ae6c8928e7d73b85a03efa1a189f3
AJ <58134242+cs-joy@users.noreply.github.com>
2021-11-08 18:51:07 -0500

    finally available v0.1.5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
-import App from './cApp';
+import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
